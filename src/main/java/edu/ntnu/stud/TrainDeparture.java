0782ff196a81e591d3a8a1f337b1bf4c64716f5e
jonathan <jonathhu@stud.ntnu.no>
2023-10-21 19:01:24 +0200

    Made constructor with appropriate javadoc

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -1,4 +1,66 @@
 package edu.ntnu.stud;
 
+import java.time.LocalTime;
+
+/**
+ * Represents a train departure.
+ *
+ * <p>Role/Responsibility:</p>
+ * This class acts as a model for a single train departure.
+ * It stores the departure time, line, train number, destination, track and delay of a train
+ *
+ * <p>Data and Data Types:</p>
+ * - departureTime (Localtime): The departure time for the train.
+ * - line (String): The line the train is on.
+ * - trainNumber (int): The train number.
+ * - destination (String): The destination of the train.
+ * - track (String): The track the train departs from.
+ * - delay (LocalTime): The delay of the train, if any.
+ *
+ * <p>Reasons for Data Types Choices:</p>
+ * - departureTime: LocalTime is a good choice because it is naturally represents a time of day
+ * without a date.
+ * - line: String is suitable for general text.
+ * - trainNumber: int is suitable for this number.
+ * - destination: String is suitable for general text.
+ * - track: int is suitable for this number.
+ * - delay: Chose LocalTime so that it is easy to add to the departure time.
+ *
+ * <p>Immutable Data upon Creation:</p>
+ * departureTime, line, trainNumber and destination are immutable upon creation. This is because
+ * they won't ever change.
+ *
+ * <p>Handling Invalid Data </p>
+ * ...
+ */
+
 public class TrainDeparture {
+
+  private final LocalTime departureTime;
+  private final String line;
+  private final int trainNumber;
+  private final String destination;
+  private int track;
+  private LocalTime delay;
+
+  /**
+   * Constructor for TrainDeparture.
+   *
+   * @param departureTime the departure time. Immutable once initialized.
+   * @param line          the line. Immutable once initialized.
+   * @param trainNumber   the train number. Immutable once initialized.
+   * @param destination   the destination. Immutable once initialized.
+   * @param track         the track. Mutable.
+   * @param delay         the delay. Mutable.
+   */
+  public TrainDeparture(LocalTime departureTime, String line, int trainNumber, String destination,
+      int track, LocalTime delay) {
+    this.departureTime = departureTime;
+    this.line = line;
+    this.trainNumber = trainNumber;
+    this.destination = destination;
+    this.track = track;
+    this.delay = delay;
+  }
+
 }
