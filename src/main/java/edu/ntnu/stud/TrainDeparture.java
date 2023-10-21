a72ec1f3470c98441b2c1c497d13ad1409a00b7d
jonathan <jonathhu@stud.ntnu.no>
2023-10-21 19:10:05 +0200

    Made getters and setters with appropriate javadoc

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -6,25 +6,20 @@ import java.time.LocalTime;
  * Represents a train departure.
  *
  * <p>Role/Responsibility:</p>
- * This class acts as a model for a single train departure.
- * It stores the departure time, line, train number, destination, track and delay of a train
+ * This class acts as a model for a single train departure. It stores the departure time, line,
+ * train number, destination, track and delay of a train
  *
  * <p>Data and Data Types:</p>
- * - departureTime (Localtime): The departure time for the train.
- * - line (String): The line the train is on.
- * - trainNumber (int): The train number.
- * - destination (String): The destination of the train.
- * - track (String): The track the train departs from.
- * - delay (LocalTime): The delay of the train, if any.
+ * - departureTime (Localtime): The departure time for the train. - line (String): The line the
+ * train is on. - trainNumber (int): The train number. - destination (String): The destination of
+ * the train. - track (String): The track the train departs from. - delay (LocalTime): The delay of
+ * the train, if any.
  *
  * <p>Reasons for Data Types Choices:</p>
  * - departureTime: LocalTime is a good choice because it is naturally represents a time of day
- * without a date.
- * - line: String is suitable for general text.
- * - trainNumber: int is suitable for this number.
- * - destination: String is suitable for general text.
- * - track: int is suitable for this number.
- * - delay: Chose LocalTime so that it is easy to add to the departure time.
+ * without a date. - line: String is suitable for general text. - trainNumber: int is suitable for
+ * this number. - destination: String is suitable for general text. - track: int is suitable for
+ * this number. - delay: Chose LocalTime so that it is easy to add to the departure time.
  *
  * <p>Immutable Data upon Creation:</p>
  * departureTime, line, trainNumber and destination are immutable upon creation. This is because
@@ -62,5 +57,82 @@ public class TrainDeparture {
     this.track = track;
     this.delay = delay;
   }
+
+  // Getter methods
+
+  /**
+   * Gets the departure time of the train.
+   *
+   * @return the departure time.
+   */
+  public LocalTime getDepartureTime() {
+    return departureTime;
+  }
+
+  /**
+   * Gets the line of the train.
+   *
+   * @return the line.
+   */
+  public String getLine() {
+    return line;
+  }
+
+  /**
+   * Gets the train number.
+   *
+   * @return the train number.
+   */
+  public int getTrainNumber() {
+    return trainNumber;
+  }
+
+  /**
+   * Gets the destination of the train.
+   *
+   * @return the destination.
+   */
+  public String getDestination() {
+    return destination;
+  }
+
+  /**
+   * Gets the track of the train.
+   *
+   * @return the track.
+   */
+  public int getTrack() {
+    return track;
+  }
+
+  /**
+   * Gets the delay of the train.
+   *
+   * @return the delay.
+   */
+  public LocalTime getDelay() {
+    return delay;
+  }
+
+  // Setter methods
+
+  /**
+   * Sets the track of the train.
+   *
+   * @param track the track.
+   */
+  public void setTrack(int track) {
+    this.track = track;
+  }
+
+  /**
+   * Sets the delay of the train.
+   *
+   * @param delay the delay.
+   */
+  public void setDelay(LocalTime delay) {
+    this.delay = delay;
+  }
+
 
 }
