960ce892eba23b246fe098b46eedbd0fd6dd6499
jonathan <jonathhu@stud.ntnu.no>
2023-11-14 15:04:38 +0100

    Register

---
+++ src/main/java/edu/ntnu/stud/TrainDepartureRegister.java
@@ -0,0 +1,12 @@
+package edu.ntnu.stud;
+
+import java.util.HashMap;
+
+public class TrainDepartureRegister {
+
+  private final HashMap<Integer, TrainDeparture> register;
+
+  public TrainDepartureRegister() {
+    this.register = new HashMap<>();
+  }
+}
